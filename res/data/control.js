bc8e299b0704fcd6ceff88f87201bf733ca19451
achmadthorik <achmadthorik2@gmail.com>
2021-10-05 15:09:16 +0700

    new commit

---
+++ res/data/control.js
@@ -0,0 +1,29 @@
+var devices = [
+    {
+        "name": "DEVICE 01",
+        "address": "192.168.1.60",
+        "topic": "device-1",
+        "battery": 40,
+        "water_level": 90,
+        "interval": 3,
+        "proximity": true
+    },
+    {
+        "name": "DEVICE 02",
+        "address": "192.168.1.3",
+        "topic": "device-2",
+        "battery": 20,
+        "water_level": 10,
+        "interval": 3,
+        "proximity": false
+    },
+    {
+        "name": "DEVICE 03",
+        "address": "192.168.1.155",
+        "topic": "device-3",
+        "battery": 100,
+        "water_level": 100,
+        "interval": 3,
+        "proximity": true
+    }
+];
